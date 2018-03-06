c60ec0da15b113eb18a676bdcaa43ce49eaba1dc
Paulius Kigas <paulius.kigas@gmail.com>
2018-03-06 14:20:43 +0200

    Add fasd

---
+++ zsh/user/fasd.zsh
@@ -0,0 +1,7 @@
+PATH="$HOME/.zh/fasd:$PATH"
+fasd_cache="$HOME/.fasd-init-bash"
+if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+  fasd --init auto >| "$fasd_cache"
+fi
+source "$fasd_cache"
+unset fasd_cache
--- zshrc
+++ zshrc
@@ -27,6 +27,7 @@ source ~/.zsh/user/rprompt.zsh
 source ~/.zsh/user/functions.zsh
 source ~/.zsh/user/tab-title.zsh
 source ~/.zsh/user/nvm.zsh
+source ~/.zsh/user/fasd.zsh
 
 source ~/.fzf.zsh
 source ~/.iterm2_shell_integration.zsh

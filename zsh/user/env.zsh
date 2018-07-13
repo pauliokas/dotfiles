3a8e7c1ac25a45dcca91908a5c47862dfb2551c8
Paulius Kigas <paulius.kigas@gmail.com>
2018-07-13 19:37:14 +0300

    Change color for zsh-autosuggestions

--- zsh/user/env.zsh
+++ zsh/user/env.zsh
@@ -2,6 +2,7 @@ export EDITOR='vim'
 export PIP_REQUIRE_VIRTUALENV=true
 export GOPATH="${HOME}/Projects/golang"
 export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=14"
 
 if [[ -d "${HOME}/bin" ]]; then
 	export PATH="${HOME}/bin:${PATH}"

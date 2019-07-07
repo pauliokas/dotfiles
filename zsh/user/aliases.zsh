651b2fc7431e5c6751d3e5411d4289c60bd053b6
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2019-07-07 13:51:47 +0100

    Update configuration

--- zsh/user/aliases.zsh
+++ zsh/user/aliases.zsh
@@ -2,3 +2,4 @@ alias ping="prettyping --nolegend"
 alias top=htop
 alias cat=bat
 alias hostmaker-aws="source ~/Projects/Hostmaker/handbook/scripts/mfa.sh hostmaker"
+alias yay="yay --pacman=powerpill"

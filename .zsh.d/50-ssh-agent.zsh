75566f2e7dc8fccf9acfc5d7d82aabd96d28131d
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2019-07-09 11:14:48 +0300

    Update config

--- .zsh.d/50-ssh-agent.zsh
+++ .zsh.d/50-ssh-agent.zsh
@@ -1,13 +1,2 @@
-function () {
-    local environment_path="$HOME/.ssh/environment"
-
-    if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-        ssh-agent > $environment_path
-    fi
-
-    chmod 600 $environment_path
-
-    if [[ ! "$SSH_AUTH_SOCK" ]]; then
-        eval "$(<$environment_path)" >/dev/null
-    fi
-}
+eval $(gnome-keyring-daemon --start)
+export SSH_AUTH_SOCK

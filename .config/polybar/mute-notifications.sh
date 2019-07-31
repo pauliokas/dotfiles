d9d904a5a9d5f9cc6df7243ffe25141e9c42039d
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2019-07-31 18:49:33 +0300

    Update configuration

---
+++ .config/polybar/mute-notifications.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+silent='false'
+
+function print_status() {
+    if [[ $silent = 'true' ]]
+    then
+        echo ''
+    else
+        echo ''
+    fi
+}
+
+function toggle() {
+    if [[ $silent = 'true' ]]
+    then
+        silent='false'
+        killall -SIGUSR2 dunst
+    else
+        silent='true'
+        killall -SIGUSR1 dunst
+    fi
+    print_status
+}
+
+trap cleanup EXIT
+trap toggle USR1
+
+while true
+do
+    print_status
+    sleep 1 &
+    wait
+done

7ea89d6eef8c2a2a58212e9628371572604df6be
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2019-07-31 16:50:41 +0300

    Stop using powerpill

--- .zsh.d/50-aliases.zsh
+++ .zsh.d/50-aliases.zsh
@@ -2,5 +2,4 @@ alias ping="prettyping --nolegend"
 alias top=htop
 alias cat=bat
 alias hostmaker-aws="source ~/Projects/Hostmaker/handbook/scripts/mfa.sh hostmaker"
-alias yay="yay --pacman=powerpill"
 alias dotfiles='git --git-dir=$HOME/.dotgit --work-tree=$HOME'

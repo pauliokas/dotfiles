80fb005fb7927e6ed9bd68f3f373ace7fccaf7e2
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2019-08-01 18:00:34 +0300

    Use standard checkupdates script for polybar

--- .config/polybar/checkupdates.sh
+++ .config/polybar/checkupdates.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 
-tmppath="${TMPDIR:-/tmp}/yay-checkup-db-${USER}"
-dbpath="$(pacman-conf DBPath)"
-
-trap 'rm -f $tmppath/db.lck' INT TERM EXIT
-
-mkdir -p "$tmppath"
-ln -s "$dbpath/local" "$tmppath" &>/dev/null
-fakeroot -- yay -Sy --dbpath "$tmppath" --logfile /dev/null &>/dev/null
-
-update_count=$(yay -Qu --dbpath "$tmppath" 2>/dev/null | wc -l)
+update_count=$(checkupdates | wc -l)
 if [ $update_count -eq 0 ]
 then
     echo ''

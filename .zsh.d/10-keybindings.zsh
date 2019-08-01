c166773109fcf9c311a519ce223b92f71aa0eaae
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2019-08-01 17:50:29 +0300

    Make Alt+arrows skip words on zsh

--- .zsh.d/10-keybindings.zsh
+++ .zsh.d/10-keybindings.zsh
@@ -4,3 +4,5 @@ zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 bindkey "^[[A" up-line-or-beginning-search
 bindkey "^[[B" down-line-or-beginning-search
+bindkey "^[[1;3C" forward-word
+bindkey "^[[1;3D" backward-word

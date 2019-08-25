d138381846495a215b931c0efa0dc42ce6287fe1
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2019-08-25 15:56:51 +0300

    Refresh prompt

--- .zsh.d/50-prompt.zsh
+++ .zsh.d/50-prompt.zsh
@@ -4,5 +4,6 @@ promptinit
 export ZSH_THEME_GIT_PROMPT_CACHE=1
 source "${HOME}/.zsh.d/zsh-git-prompt/zshrc.sh"
 
-PROMPT=" %~ %# "
-RPROMPT=$(git_super_status)
+PROMPT='
+┌ %~ $(git_super_status)
+└ %# '

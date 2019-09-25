f68e8de9a412b8f2e005fe6608669ff5523a4244
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2019-09-25 17:59:41 +0300

    Add yarn alias to handle nvmrc automatically

--- zsh.d/50-nvm.zsh
+++ zsh.d/50-nvm.zsh
@@ -4,3 +4,12 @@ function nvm() {
   source "/usr/share/nvm/nvm.sh"
   nvm "$@"
 }
+
+yarn_orig="$(which yarn)"
+function yarn() {
+  if [[ -f "$(pwd)/.nvmrc" && "$(nvm version $(cat $(pwd)/.nvmrc))" != "$(nvm current)" ]]; then
+    nvm use
+  fi
+
+  $yarn_orig "$@"
+}

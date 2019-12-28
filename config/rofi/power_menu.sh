b8ee5b52ed91c842b9fe4e1a94442a92ea3fcb5c
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2019-12-28 15:36:51 +0200

    Power menu for rofi

---
+++ config/rofi/power_menu.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+suspend=''
+lock=''
+restart=''
+shutdown=''
+
+choice=`rofi \
+    -dmenu \
+    -theme $HOME/.config/rofi/themes/tiled-view.rasi <<END
+$suspend
+$lock
+$restart
+$shutdown
+END`
+
+case $choice in
+    $suspend)
+        systemctl suspend
+        ;;
+    $lock)
+        loginctl lock-session
+        ;;
+    $restart)
+        systemctl reboot
+        ;;
+    $shutdown)
+        systemctl poweroff
+        ;;
+esac

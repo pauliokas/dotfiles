e148e67599b6ccec5e4f5493f8694175e7a784f9
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2019-12-28 16:13:05 +0200

    Review code

--- config/rofi/power_menu.sh
+++ config/rofi/power_menu.sh
@@ -5,26 +5,27 @@ lock=''
 restart=''
 shutdown=''
 
-choice=`rofi \
-    -dmenu \
-    -theme $HOME/.config/rofi/themes/tiled-view.rasi <<END
-$suspend
-$lock
-$restart
-$shutdown
-END`
+choice=$(rofi \
+	-dmenu \
+	-theme $HOME/.config/rofi/themes/tiled-view.rasi <<- END
+		$suspend
+		$lock
+		$restart
+		$shutdown
+	END
+)
 
 case $choice in
-    $suspend)
-        systemctl suspend
-        ;;
-    $lock)
-        loginctl lock-session
-        ;;
-    $restart)
-        systemctl reboot
-        ;;
-    $shutdown)
-        systemctl poweroff
-        ;;
+	$suspend)
+		systemctl suspend
+		;;
+	$lock)
+		loginctl lock-session
+		;;
+	$restart)
+		systemctl reboot
+		;;
+	$shutdown)
+		systemctl poweroff
+		;;
 esac

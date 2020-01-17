d26679e29af6169788f9649b07f9397fefaaf512
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2020-01-17 17:39:16 +0200

    Revisit nvm setup

--- zsh.d/50-nvm.zsh
+++ zsh.d/50-nvm.zsh
@@ -1,15 +1,23 @@
 export NVM_DIR="${HOME}/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh" --no-use
 
-function nvm() {
-  source "/usr/share/nvm/nvm.sh"
-  nvm "$@"
-}
+# https://github.com/nvm-sh/nvm#calling-nvm-use-automatically-in-a-directory-with-a-nvmrc-file
+autoload -U add-zsh-hook
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
 
-yarn_orig="$(which yarn)"
-function yarn() {
-  if [ -f "$(pwd)/.nvmrc" ] && [ "$(nvm version $(cat $(pwd)/.nvmrc))" != "$(nvm current)" ]; then
-    nvm use
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use >/dev/null
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    nvm use default >/dev/null
   fi
-
-  $yarn_orig "$@"
 }
+add-zsh-hook chpwd load-nvmrc
+[ -s "$(nvm_find_nvmrc)" ] && load-nvmrc

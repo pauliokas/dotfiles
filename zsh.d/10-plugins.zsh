2ba91707f93ce5b9daa002352daff45dc832c551
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2020-01-17 19:58:07 +0200

    Revisit zsh prompt

--- zsh.d/10-plugins.zsh
+++ zsh.d/10-plugins.zsh
@@ -1,4 +1,3 @@
-source $HOME/.zsh.d/zsh-git-prompt/zshrc.sh
 source $HOME/.zsh.d/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $HOME/.zsh.d/oh-my-zsh/plugins/docker-compose/docker-compose.plugin.zsh
 source $HOME/.zsh.d/oh-my-zsh/plugins/yarn/yarn.plugin.zsh
--- zsh.d/50-prompt.zsh
+++ zsh.d/50-prompt.zsh
@@ -1,9 +1,30 @@
 autoload -Uz promptinit
 promptinit
 
-export ZSH_THEME_GIT_PROMPT_CACHE=1
-source "${HOME}/.zsh.d/zsh-git-prompt/zshrc.sh"
+source "${HOME}/.zsh.d/gitstatus/gitstatus.prompt.zsh"
 
-PROMPT='
-┌ %~ $(git_super_status)
-└ %# '
+_last_command_started=0
+
+PROMPT=''
+PROMPT+='%(0?::%F{red}(%?%)%f) '
+PROMPT+='%F{blue}%(5~:%-1~/../%2~:%~)%f '
+PROMPT+='%($((_last_command_started + 5))S:%F{red}$(( SECONDS - _last_command_started ))%F{yellow}s%f :)'
+PROMPT+='%(!:%F{red}:%F{green})%#%f '
+
+RPROMPT='$GITSTATUS_PROMPT'
+
+autoload -Uz add-zsh-hook
+
+function _newline_before_prompt() {
+  if [ -z "$_print_newline_before_prompt" ]; then
+    _print_newline_before_prompt=1
+  elif [ "$_print_newline_before_prompt" -eq 1 ]; then
+    printf '\n'
+  fi
+}
+add-zsh-hook precmd _newline_before_prompt
+
+function _save_last_command_start() {
+  _last_command_started=$SECONDS
+}
+add-zsh-hook preexec _save_last_command_start

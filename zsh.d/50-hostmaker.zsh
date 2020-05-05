c92f9a92cebbb91cb97f9dfdfe2b06360e0ed5d0
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2020-05-05 11:44:53 +0300

    Remove hostmaker specific things

--- zsh.d/50-hostmaker.zsh
+++
@@ -1,6 +0,0 @@
-function hostmaker-aws {
-	aws-mfa --profile=hostmaker --short-term-suffix=2fa --token="$(1psw get totp 'AWS - Hostmaker')"
-	export AWS_PROFILE=hostmaker-staging
-}
-export ANDROID_HOME="$HOME/.android/Sdk"
-export JAVA_HOME="/usr/lib/jvm/default"

6b9445426d832dc0db2dfcc1b586bc75c2113cc1
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2020-09-01 14:27:23 +0300

    Add yarn autocompletion

--- zsh.d/01-fpath-completion.zsh
+++ zsh.d/01-fpath-completion.zsh
@@ -2,6 +2,7 @@ fpath=(
   $HOME/.zsh.d/oh-my-zsh/plugins/docker
   $HOME/.zsh.d/oh-my-zsh/plugins/docker-compose
   $HOME/.zsh.d/oh-my-zsh/plugins/nvm
+  $HOME/.zsh.d/oh-my-zsh/plugins/yarn
   ${fpath}
 )
 

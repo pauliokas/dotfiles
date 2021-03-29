f8423486df324d5e2550dfdde3afbad6f9932fe6
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2021-03-29 19:13:12 +0300

    Enable emacs mode on zsh

--- zsh.d/10-keybindings.zsh
+++ zsh.d/10-keybindings.zsh
@@ -2,9 +2,4 @@ autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
-bindkey "^[[A" up-line-or-beginning-search
-bindkey "^[[B" down-line-or-beginning-search
-bindkey "^[[1;3C" forward-word
-bindkey "^[[1;3D" backward-word
-bindkey "[C" forward-word
-bindkey "[D" backward-word
+bindkey -e

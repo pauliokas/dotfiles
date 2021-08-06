cfcbf232267b6d0cc058ca2b2171507ea6430e96
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2021-08-06 19:05:42 +0300

    Add DOOM Emacs

--- zsh.d/50-env.zsh
+++ zsh.d/50-env.zsh
@@ -3,3 +3,7 @@ export EDITOR='vim'
 if [[ -d "${HOME}/.local/bin" ]]; then
 	export PATH="${HOME}/.local/bin:${PATH}"
 fi
+
+if [[ -d "${HOME}/.config/emacs/bin" ]]; then
+	export PATH="${HOME}/.config/emacs/bin:${PATH}"
+fi

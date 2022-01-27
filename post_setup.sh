dcc145576b64fe16335236a6f9d32100958cddd2
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2022-01-27 11:28:15 +0200

    Update configuration

--- post_setup.sh
+++ post_setup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-if [ $(command -v bat >/dev/null) ]; then
+if command -v bat >/dev/null; then
     printf 'Building bat cache... '
 	bat cache --build >/dev/null
+	printf 'OK\n'
+fi
+
+if command -v nvim >/dev/null; then
+    printf 'Linking nvim to vi and vim...\n'
+	sudo ln -sf $(which nvim) /usr/local/bin/vi
+	sudo ln -sf $(which nvim) /usr/local/bin/vim
 	printf 'OK\n'
 fi

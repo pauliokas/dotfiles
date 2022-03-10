e6f68870a0f430d2c634d75912fef3ed75e5271d
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2022-03-10 18:24:12 +0200

    Tweak fasd

--- zsh.d/50-fasd.zsh
+++ zsh.d/50-fasd.zsh
@@ -1,6 +1,6 @@
 command -v fasd >/dev/null || return
 
-fasd_cache="$HOME/.fasd-init-bash"
+fasd_cache="${XDG_CONFIG_CACHE:-$HOME/.cache/fasd/init}"
 if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
   fasd --init auto >| "$fasd_cache"
 fi

98a11de54585f0e294865b8c02729118a7da9f1c
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2024-11-16 15:46:39 +0200

    Update nvim config

--- config/nvim/lua/plugins/catppuccin.lua
+++ config/nvim/lua/plugins/catppuccin.lua
@@ -2,11 +2,12 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
-  opts = {
-    flavour = "auto",
-    background = {},
-  },
   config = function()
+    require("catppuccin").setup({
+      flavour = "auto",
+      background = {},
+      transparent_background = true,
+    })
     vim.cmd.colorscheme('catppuccin')
   end
 }

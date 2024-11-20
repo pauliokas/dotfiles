ffce596fdb677debe7778db596b27e6af7ee574f
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2024-11-20 15:08:19 +0200

    Update nvim config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -21,22 +21,41 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
       "williamboman/mason-lspconfig.nvim",
+      "kevinhwang91/nvim-ufo",
     },
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true
+      }
 
       local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities
-      })
+      local language_servers = lspconfig.util.available_servers()
+      for _, ls in ipairs(language_servers) do
+        lspconfig[ls].setup({
+          capabilities = capabilities
+        })
+      end
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  },
+
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = "kevinhwang91/promise-async",
+    config = function ()
+      require("ufo").setup({
+        close_fold_kinds_for_ft = {
+          default = {'imports'},
+        }
+      })
+
+      vim.o.foldcolumn = "1"
     end
   },
 }
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,47 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function ()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = {},
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          node_incremental = "v",
-          node_decremental = "V",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-refactor",
+    },
+    build = ":TSUpdate",
+    config = function ()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {},
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
         },
-      },
-    })
-  end
+        indent = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            node_incremental = "v",
+            node_decremental = "V",
+          },
+        },
+        refactor = {
+          highlight_definitions = {
+            enable = true,
+            clear_on_cursor_move = true,
+          },
+          highlight_current_scope = {
+            enable = true,
+          },
+          smart_rename = {
+            enable = true,
+            keymaps = {
+              smart_rename = "grr",
+            },
+          },
+        },
+      })
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+  },
 }

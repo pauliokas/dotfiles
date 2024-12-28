dc37ced0c4a9f8d562589917187dfa60bd814851
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2024-12-28 14:18:00 +0200

    Update zsh config

--- config/zsh/plugins/asdf/asdf.zsh
+++ config/zsh/plugins/asdf/asdf.zsh
@@ -1,4 +1,4 @@
-command -v fzf >/dev/null || return 0
+command -v asdf >/dev/null || return 0
 
 # export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME:-$HOME/.config}/asdf-vm/.asdfrc"
 # export ASDF_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/asdf-vm/.asdfrc"
@@ -6,4 +6,4 @@ command -v fzf >/dev/null || return 0
 export ASDF_DIR="$(brew --prefix asdf)/libexec"
 export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME:-$HOME/.config}/asdf-vm/.asdfrc"
 export ASDF_DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/asdf-vm"
-source "$(brew --prefix asdf)/libexec/asdf.sh"
+#source "$(brew --prefix asdf)/libexec/asdf.sh"

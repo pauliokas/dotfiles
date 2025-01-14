3d776702090f437e1044c69e39588bfe7a80f149
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2025-01-14 11:32:05 +0200

    Alias zoxide to cd

--- config/zsh/plugins/zoxide/zoxide.zsh
+++ config/zsh/plugins/zoxide/zoxide.zsh
@@ -1,4 +1,3 @@
 command -v zoxide >/dev/null || return 0
 
-source <(_ZO_DATA_DIR="${XDG_DATA_DIR:-$HOME/.local/share}" zoxide init zsh)
-
+source <(_ZO_DATA_DIR="${XDG_DATA_DIR:-$HOME/.local/share}" zoxide init zsh --cmd cd)

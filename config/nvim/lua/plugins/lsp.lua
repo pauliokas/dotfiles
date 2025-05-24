77a67e4081c06292a33eead05d3914859d24ec3f
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2025-05-24 15:43:23 +0300

    Update nvim config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     build = ":MasonUpdate",
     config = function(_, opts)
       require("mason").setup(opts)
@@ -8,9 +8,32 @@ return {
   },
 
   {
-    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
     dependencies = {
-      "williamboman/mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
+      "kevinhwang91/nvim-ufo",
+      "saghen/blink.cmp",
+    },
+    config = function(_, opts)
+      -- local capabilities = require('blink.cmp').get_lsp_capabilities()
+      --
+      -- require("mason-lspconfig").setup_handlers({
+      --   function (server_name)
+      --     print(capabilities)
+      --     vim.lsp.config[server_name] = { capabilities = capabilities }
+      --   end
+      -- })
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Show declaration' })
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
+    end
+  },
+
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mason-org/mason.nvim",
     },
     opts = {
       automatic_installation = true,
@@ -21,35 +44,8 @@ return {
         "pylsp",
         "terraformls",
         "ts_ls",
+        "gopls",
       },
-
-      -- handlers = {
-      --   function (server_name)
-      --     local capabilities = require('blink.cmp').get_lsp_capabilities()
-      --     require('lspconfig')[server_name].setup({ capabilities = capabilities })
-      --   end,
-      -- },
     },
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "kevinhwang91/nvim-ufo",
-      "saghen/blink.cmp",
-    },
-    config = function(_, opts)
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-      require("mason-lspconfig").setup_handlers({
-        function (server_name)
-          require("lspconfig")[server_name].setup({ capabilities = capabilities })
-        end
-      })
-
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Show declaration' })
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
-    end
   },
 }
-
